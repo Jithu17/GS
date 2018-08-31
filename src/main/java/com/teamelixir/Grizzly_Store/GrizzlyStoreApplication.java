af4fbf10abf1a9e8d5393bbb84825548d517bb4d
Jithu <723312@1000810002149M.local>
2018-08-31 14:24:50 +1000

    First commit

---
+++ src/main/java/com/teamelixir/Grizzly_Store/GrizzlyStoreApplication.java
@@ -0,0 +1,12 @@
+package com.teamelixir.Grizzly_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizzlyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrizzlyStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/teamelixir/Grizzly_Store/GrizzlyStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teamelixir.Grizzly_Store;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrizzlyStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
